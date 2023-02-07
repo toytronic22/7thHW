1028189d437e4c6a748f62c8a8c75ef90c53f9be
toytronic22 <leshaklew@gmail.com>
2023-02-07 16:37:36 +0300

    added tag

--- src/test/java/simple/NegativeTests.java
+++ src/test/java/simple/NegativeTests.java
@@ -1,11 +1,12 @@
 package simple;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@Tag("simple")
 public class NegativeTests {
     @Test
     @Disabled
